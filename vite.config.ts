6351ce5dfa9ba78245171d46611e903bdc3ff0f2
Marshal <wayansmarshalls18@gmail.com>
2026-06-10 15:48:59 +0300

    Update: updated the scripts to be available to the github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/marshall-ai/',
+  base: '/Marshall-AI/',
   plugins: [react()],
   resolve: {
     alias: {
